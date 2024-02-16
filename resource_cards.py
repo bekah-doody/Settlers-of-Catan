08abc7784d0f6d0e36e072c778d7b1432c7f1b6b
bekah-doody <doodyr@mail.gvsu.edu>
2024-02-16 10:21:54 -0500

    Added Card enum class to resource_cards

--- resource_cards.py
+++ resource_cards.py
@@ -1,14 +1,22 @@
+from enum import Enum
+
+
+class Card(Enum):
+    WOOD = 0
+    BRICK = 1
+    WOOL = 2
+    WHEAT = 3
+    ORE = 4
 
 class Resource_Cards:
 
-    def __init__(self, type: str):
+    def __init__(self, type: Card):
         self.__type = type
-        self.__name = type
 
     @property
-    def name(self) -> str:
-        return self.__name
+    def type(self) -> str:
+        return self.__type
 
-    @name.setter
-    def name(self, type):
-        self.__name = type
+    @type.setter
+    def name(self, type: Card):
+        self.__type = type
