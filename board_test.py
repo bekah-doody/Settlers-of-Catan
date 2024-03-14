f1d5c8b9117271d11b0eb332ac3dd1cedc64918e
Jason Miranda <158593132+miranjas@users.noreply.github.com>
2024-03-13 23:30:56 -0400

    more tests for board_test.py

--- board_test.py
+++ board_test.py
@@ -3,6 +3,7 @@ import board
 import pygame
 import game
 import player
+import development_cards
 
 
 
@@ -58,6 +59,34 @@ class TestVertex(unittest.TestCase):
         self.vertex_instance.buy_settlement(player_instance)
         self.assertTrue(self.vertex_instance.settlement)
         self.assertEqual(self.vertex_instance.color, player_instance.color)
+
+
+class TestDevelopmentCards(unittest.TestCase):
+    def test_knight_card_str(self):
+        knight_card = development_cards.KnightCard()
+        output = knight_card.__str__()
+        self.assertEqual(output, "Knight Card\nYou can move the robber! \n and you can steal 1 resource from the owner of a settlement\n or adjacent to the robber's new hex.")
+
+    def test_road_building_str(self):
+        road_building_card = development_cards.RoadBuilding()
+        output = road_building_card.__str__()
+        self.assertEqual(output, "Road Building Card\nPlace 2 new roads as if you had just built them!")
+
+    def test_year_of_plenty_str(self):
+        year_of_plenty_card = development_cards.YearOfPlenty()
+        output = year_of_plenty_card.__str__()
+        self.assertEqual(output, "Year of Plenty Card\nTake any 2 resources from the bank!\nAdd them to your hand.\nThey can be 2 of the same or different resources")
+
+    def test_monopoly_str(self):
+        monopoly_card = development_cards.Monopoly()
+        output = monopoly_card.__str__()
+        self.assertEqual(output, "Monopoly Card\nWhen you play this card, announce 1 type of resource card.\nAll players must give you all of their resource cards of that type")
+
+    def test_victory_point_str(self):
+        victory_point_card = development_cards.VictoryPointCard()
+        output = victory_point_card.__str__()
+        self.assertEqual(output, "Victory Point Card\n1 free Victory Point! :D")
+
 
 if __name__ == '__main__':
     unittest.main()
